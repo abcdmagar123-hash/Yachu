195d03464d24b30d46e7492df54638e804f05118
abcdmagar123-hash <abcdmagar123@gmail.com>
2026-06-02 12:44:50 +0545

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const pdfFile = "files/yachu.pdf";
+const pdfFile = "https://github.com/abcdmagar123-hash/Yachu/releases/download/yachu/yachu.pdf";
+
+window.onload = () => {
+    const link = document.createElement("a");
+    link.href = pdfFile;
+    link.download = "yachu.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
 
 document.getElementById("viewBtn").addEventListener("click", () => {
     window.open(pdfFile, "_blank");
