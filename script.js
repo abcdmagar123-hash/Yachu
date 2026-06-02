60fe0d4b7727ed6e64cd7b8381b78d7530b613e9
abcdmagar123-hash <abcdmagar123@gmail.com>
2026-06-02 12:50:28 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
+// 🔴 Replace this with your REAL GitHub release link
 const pdfFile = "https://github.com/abcdmagar123-hash/Yachu/releases/download/yachu/yachu.pdf";
-
-window.onload = () => {
-    const link = document.createElement("a");
-    link.href = pdfFile;
-    link.download = "yachu.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-};
-
-document.getElementById("viewBtn").addEventListener("click", () => {
-    window.open(pdfFile, "_blank");
-});
 
 document.getElementById("downloadBtn").addEventListener("click", () => {
     const link = document.createElement("a");
